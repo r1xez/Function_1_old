26574ed236f8ec924f59efa780c8c12bcdcaf08b
r1xez <164931719+r1xez@users.noreply.github.com>
2024-10-31 21:29:38 +0200

    completed

--- Function_1_old/main.cpp
+++ Function_1_old/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 using namespace std;
 
+bool isPerfect(int num) {
+    int sum = 0;
+    for (int i = 1; i <= num / 2; ++i) {
+        if (num % i == 0) sum += i;
+    }
+    return sum == num;
+}
+
+void findPerfectNumbers(int start, int end) {
+    for (int i = start; i <= end; ++i) {
+        if (isPerfect(i)) cout << i << " ";
+    }
+    cout << endl;
+}
+
 int main() {
-	cout << "Hello, World!" << endl;
-	return 0;
+    int start, end;
+    cout << "Enter interval start and end: ";
+    cin >> start >> end;
+    cout << "Perfect numbers: ";
+    findPerfectNumbers(start, end);
+    return 0;
 }
