221f7714a3bdbd605f1942c3e0e76b7536daef7c
r1xez <164931719+r1xez@users.noreply.github.com>
2024-10-31 21:28:45 +0200

    completed

--- Function_1_old/main.cpp
+++ Function_1_old/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int rangeSum(int a, int b) {
+    int sum = 0;
+    for (int i = min(a, b); i <= max(a, b); ++i) {
+        sum += i;
+    }
+    return sum;
+}
+
 int main() {
-	cout << "Hello, World!" << endl;
-	return 0;
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "Sum of range: " << rangeSum(a, b) << endl;
+    return 0;
 }
