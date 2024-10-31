ad2cbc9e2cecb4ea2e6474340323f34d992d160d
r1xez <164931719+r1xez@users.noreply.github.com>
2024-10-31 21:31:24 +0200

    completed

--- Function_1_old/main.cpp
+++ Function_1_old/main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 using namespace std;
 
+bool isLucky(int number) {
+    if (number < 100000 || number > 999999) return false;
+
+    int firstHalf = number / 1000;
+    int secondHalf = number % 1000;
+
+    int sum1 = 0, sum2 = 0;
+    while (firstHalf > 0) {
+        sum1 += firstHalf % 10;
+        firstHalf /= 10;
+    }
+    while (secondHalf > 0) {
+        sum2 += secondHalf % 10;
+        secondHalf /= 10;
+    }
+
+    return sum1 == sum2;
+}
+
 int main() {
-	cout << "Hello, World!" << endl;
-	return 0;
+    int number;
+    cout << "Enter a six-digit number: ";
+    cin >> number;
+    if (isLucky(number))
+        cout << "The number is lucky!" << endl;
+    else
+        cout << "The number is not lucky." << endl;
+    return 0;
 }
